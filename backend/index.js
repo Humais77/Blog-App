78abc103e7c2b939d2b6c382352e7982861af01f
Humais <humaissamsung@gmail.com>
2025-08-21 13:49:52 +0500

    connect to the db

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connected to MongoDB');
+
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
 const app = express();
 
 app.listen(3000,()=>{
