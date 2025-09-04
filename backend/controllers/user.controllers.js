21b7732c4708d45fba3a1e25bf8ce9d57ca44afd
Humais <humaissamsung@gmail.com>
2025-09-04 13:45:03 +0500

    add delete user API route

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -41,11 +41,19 @@ const updateUser = async(req,res,next)=>{
             const {password,...rest}= updateUser._doc;
             res.status(200).json(rest);
         } catch (error) {
-            
+            next(error)
         }
+    
+};
+const deleteUser = async(req,res,next)=>{
+    if (req.user.id !== req.params.userId){
+        return next(errorHandler(403,'Your are not allowed to delete this user'))
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json('User has been deleted');
+    } catch (error) {
         
-    
-    
-
+    }
 }
-module.exports = { test,updateUser };
+module.exports = { test,updateUser,deleteUser };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { test, updateUser } = require('../controllers/user.controllers.js');
+const { test, updateUser, deleteUser } = require('../controllers/user.controllers.js');
 const { verifyToken } = require('../utils/verifyUser.js');
 
 const route = express.Router();
 route.get('/test',test)
 route.put('/update/:userId',verifyToken,updateUser)
+route.delete('/delete/:userId',verifyToken,deleteUser)
 module.exports = route;
