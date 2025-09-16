89d78a493c389e3105a9da5e7f614481bb204ed1
Humais <humaissamsung@gmail.com>
2025-09-16 13:33:58 +0500

    create get users API route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { test, updateUser, deleteUser, signout } = require('../controllers/user.controllers.js');
+const { test, updateUser, deleteUser, signout, getUsers } = require('../controllers/user.controllers.js');
 const { verifyToken } = require('../utils/verifyUser.js');
 
 const route = express.Router();
@@ -7,4 +7,5 @@ route.get('/test',test)
 route.put('/update/:userId',verifyToken,updateUser)
 route.delete('/delete/:userId',verifyToken,deleteUser)
 route.post('/signout',signout)
+route.get('/getusers',verifyToken,getUsers)
 module.exports = route;
